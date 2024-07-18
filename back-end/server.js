5dda0334de00079fe9cd8a4c7d3d83ed6a356022
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-07-18 20:57:43 +0300

    I haven't used these

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const fs = require("fs");
 const cors = require("cors");
 const mysql = require("mysql2");
 const bodyParser = require("body-parser");
